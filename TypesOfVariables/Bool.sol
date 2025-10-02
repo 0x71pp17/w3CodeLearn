59ef185908769363eaf6ce560f29328841e84f03
0x71pp17 <tippitj@gmail.com>
2025-10-01 22:41:52 -0400

    Create Bool.sol

---
+++ TypesOfVariables/Bool.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+/// @title Boolean
+/// @author 0x71pp17
+/// @notice A simple contract to manage a boolean state variable
+contract Boolean {
+    // State variable: Declares a boolean with default value 'false'
+    // 'public' generates an automatic getter function for external access
+    bool public myBool;
+
+    /// @notice Updates the value of `myBool`
+    /// @dev This function acts as a setter to modify the stored boolean value
+    /// @param _myBool The new boolean value to set (true or false)
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+}   
