9891096020690bedebb76759c755d19c54c8bab7
0x71pp17 <tippitj@gmail.com>
2025-10-01 22:28:27 -0400

    Update HelloWorld.sol

--- helloWorld/HelloWorld.sol
+++ helloWorld/HelloWorld.sol
@@ -1,9 +1,11 @@
-//SPDX-License-Identifier: MIT
- 
-pragma solidity 0.8.17;
- 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+/// @title HelloWorld
+/// @author Developer
+/// @notice A minimal contract that stores a public string
 contract HelloWorld {
- 
+    // State variable: Declares a string that is publicly accessible
+    // 'public' automatically generates a getter function for external reading
     string public ourString = "Hello World";
-    
-}
+}   
