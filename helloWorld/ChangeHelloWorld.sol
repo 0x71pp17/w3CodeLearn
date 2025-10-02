b959af00abd1afd0d931567edca50fa37a970e02
0x71pp17 <tippitj@gmail.com>
2025-10-01 22:23:15 -0400

    Create ChangeHelloWorld.sol

---
+++ helloWorld/ChangeHelloWorld.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.30;
+
+/// @title ChangeHelloWorld
+/// @author Developer
+/// @notice A simple contract to store and update a string value
+contract ChangeHelloWorld {
+    // State variable: Stores a string that can be accessed externally due to 'public'
+    // The compiler automatically generates a getter function for 'public' variables
+    string public myString = "Hello world";
+
+    /// @notice Updates the value of `myString`
+    /// @dev This function allows any user to change the string stored in the contract
+    /// @param _myString The new string value to set
+    function updateOurString(string memory _myString) public {
+        // Assign the new value to the state variable
+        myString = _myString;
+    }
+}   
+ 
